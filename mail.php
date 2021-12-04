e886162c0383e211c4ee2ac9bdd05eed13866c1e
Namanraj34 <80189451+Namanraj34@users.noreply.github.com>
2021-12-04 19:35:37 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,18 +1,17 @@
 <?php
 //get data from form  
 
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
+$name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "namanraj34@mail.com";
-$subject = "Mail From Webcooks institute";
-$txt ="firstName = ". $firstName . "\r\n  email = " . $email . "\r\n message =" . $message ." \r\n lastName =". $lastName;
-$headers = "From: noreply@Webcooks.com" . "\r\n" .
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("contact.html");
+header("Location:thankyou.html");
 ?>
